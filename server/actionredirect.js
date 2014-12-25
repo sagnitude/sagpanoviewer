8b9b21c1fc9cfd724780dc2acb3e589c38522ca7
sagnitude <leyicong@gmail.com>
2014-12-25 15:29:04 +0800

    bug fixed.

--- server/actionredirect.js
+++ server/actionredirect.js
@@ -20,7 +20,13 @@ app.get("/ids/:action", function(req, res){
 function getListOfPofs(){
     http.get("http://www.navior.cn:6603/ids/listPofsOfMall.action?mallId=823&operatorKey=FA07C1D5-800E-4065-8A40-7DD2D925C1A3", function(res){
         console.log("res got");
-        return res;
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+            return trunk;
+        });
+        //return res;
     });
 }
 
