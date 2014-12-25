0764d936b8d4b023b41c3f4f6e30072a1b274fd8
sagnitude <leyicong@gmail.com>
2014-12-25 15:31:57 +0800

    typo bug fixed.

--- server/actionredirect.js
+++ server/actionredirect.js
@@ -18,15 +18,16 @@ app.get("/ids/:action", function(req, res){
 });
 
 function getListOfPofs(){
+    var result = "";;
     http.get("http://www.navior.cn:6603/ids/listPofsOfMall.action?mallId=823&operatorKey=FA07C1D5-800E-4065-8A40-7DD2D925C1A3", function(res){
         console.log("res got");
         console.log('STATUS: ' + res.statusCode);
         console.log('HEADERS: ' + JSON.stringify(res.headers));
         res.on('data', function (chunk) {
             console.log('BODY: ' + chunk);
-            return trunk;
+            result.concat(chunk);
         });
-        //return res;
+        return result;
     });
 }
 
