1dbeff1b7b6f925ca13d5885a5b3578f30bc5fe6
sagnitude <leyicong@gmail.com>
2014-12-25 15:22:36 +0800

    fetch navior action added.

--- server/actionredirect.js
+++ server/actionredirect.js
@@ -4,6 +4,7 @@
 
 var express = require("express");
 var app = express();
+var http = require("http");
 
 app.get("/", function (req, res) {
     res.send("get");
@@ -11,9 +12,16 @@ app.get("/", function (req, res) {
 
 app.get("/ids/:action", function(req, res){
     if(req.params.action == "listPofsOfMall.action"){
-        console.log("get");
+        console.log("get: " + getListOfPofs());
     }
-    res.send("response");
+    res.send("response: " + getListOfPofs());
 });
+
+function getListOfPofs(){
+    http.get("http://www.navior.cn:6603/ids/listPofsOfMall.action?mallId=823&operatorKey=FA07C1D5-800E-4065-8A40-7DD2D925C1A3", function(res){
+        console.log("res got");
+        return res;
+    });
+}
 
 app.listen(8997);
