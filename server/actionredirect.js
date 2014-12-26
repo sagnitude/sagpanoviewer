1f7b2da2b338dde4bfa56ad1379b98aae6b20a02
sagnitude <leyicong@gmail.com>
2014-12-26 14:57:57 +0800

    para added.

--- server/actionredirect.js
+++ server/actionredirect.js
@@ -12,23 +12,25 @@ app.get("/", function (req, res) {
 
 app.get("/ids/:action", function(req, res){
     if(req.params.action == "listPofsOfMall.action"){
-        getListOfPofs(res);
+        getListOfPofs(req.query, res);
     }
 });
 
-function getListOfPofs(outputPipe){
-    var req = http.request("http://www.navior.cn:6603/ids/listPofsOfMall.action?mallId=824&operatorKey=FA07C1D5-800E-4065-8A40-7DD2D925C1A3", function(res){
-        console.log('STATUS: ' + res.statusCode);
-        var result = "";
-        res.on('data', function (chunk) {
-            console.log('BODY CHUNK GOT');
-            result += chunk;
-        }).on('end', function (){
-            console.log("response from navior end: ", result);
-            outputPipe.send(JSON.parse(result));
+function getListOfPofs(queryBody, outputPipe){
+    if(queryBody.mallId && queryBody.operatorKey){
+        var req = http.request("http://www.navior.cn:6603/ids/listPofsOfMall.action?mallId="+queryBody.mallId+"&operatorKey="+queryBody.operatorKey, function(res){
+            console.log('STATUS: ' + res.statusCode);
+            var result = "";
+            res.on('data', function (chunk) {
+                console.log('BODY CHUNK GOT');
+                result += chunk;
+            }).on('end', function (){
+                outputPipe.send(JSON.parse(result));
+            });
         });
-    });
-    req.end();
+        req.write(require('querystring').stringify(queryBody));
+        req.end();
+    }
 }
 
 app.listen(8997);
