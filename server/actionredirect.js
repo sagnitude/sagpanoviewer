07679c56aab88082785c1d7ca957127e7ea9ce31
sagnitude <leyicong@gmail.com>
2014-12-29 09:36:12 +0800

    transfer for new actions added.

--- server/actionredirect.js
+++ server/actionredirect.js
@@ -12,25 +12,39 @@ app.get("/", function (req, res) {
 
 app.get("/ids/:action", function(req, res){
     if(req.params.action == "listPofsOfMall.action"){
-        getListOfPofs(req.query, res);
+        s_getListOfPofs(req.query, res);
+    }else if(req.param.action == "listMallWithFullShot.action"){
+        s_getMallWithFullShot(req.query, res);
     }
 });
 
-function getListOfPofs(queryBody, outputPipe){
+function s_getListOfPofs(queryBody, outputPipe){
     if(queryBody.mallId && queryBody.operatorKey){
-        var req = http.request("http://www.navior.cn:6603/ids/listPofsOfMall.action?mallId="+queryBody.mallId+"&operatorKey="+queryBody.operatorKey, function(res){
-            console.log('STATUS: ' + res.statusCode);
-            var result = "";
-            res.on('data', function (chunk) {
-                console.log('BODY CHUNK GOT');
-                result += chunk;
-            }).on('end', function (){
-                outputPipe.send(JSON.parse(result));
-            });
+        var reqUrl = "http://www.navior.cn:6603/ids/listPofsOfMall.action?mallId="+queryBody.mallId+"&operatorKey="+queryBody.operatorKey;
+        s_transferAction(reqUrl, queryBody, outputPipe);
+    }
+}
+
+function s_getMallWithFullShot(queryBody, outputPipe){
+    if(queryBody.mallId && queryBody.operatorKey){
+        var reqUrl = "http://www.navior.cn:6603/ids/listMallWithFullShot.action?mallId="+queryBody.mallId+"&operatorKey="+queryBody.operatorKey;
+        s_transferAction(reqUrl, queryBody, outputPipe);
+    }
+}
+
+function s_transferAction(actionUrl, queryBody, outputPipe){
+    var req = http.request(actionUrl, function(res) {
+        console.log("STATUS: " + res.statusCode);
+        var result = "";
+        res.on('data', function (chunk) {
+            console.log('BODY CHUNK GOT');
+            result += chunk;
+        }).on('end', function() {
+            outputPipe.send(JSON.parse(result));
         });
         req.write(require('querystring').stringify(queryBody));
         req.end();
-    }
+    })
 }
 
 app.listen(8997);
